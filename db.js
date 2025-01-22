24a9228fedb993d3a06846474ac5a32bde2dcee3
Luis Vallejo Aguilar <vallejo@MacBook-Pro-de-Luis.local>
2025-01-22 00:46:13 -0500

    feat: add assistent gpt

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT2,
-    ssl: {
-        rejectUnauthorized: false // Si necesitas conectarte a una base de datos sin validar el certificado
-      }
+    port: process.env.DB_PORT,
+    // ssl: {
+    //     rejectUnauthorized: false // Si necesitas conectarte a una base de datos sin validar el certificado
+    //   }
 });
 
 module.exports = pool;
